5bef5f19bdb174201d598ae3cbc5e9baa43392c7
Daniele Procida <daniele@apple-juice.co.uk>
2011-04-05 15:13:56 +0100

    more crude changes to get it working quickly

--- features/admin.py
+++ features/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from features.models import Request, Comment
-from cms.admin.placeholderadmin import PlaceholderAdmin
+# from cms.admin.placeholderadmin import PlaceholderAdmin
 
 class CommentInline(admin.TabularInline):
     model = Comment
     extra = 1
     
-class RequestAdmin(PlaceholderAdmin):
+class RequestAdmin(admin.ModelAdmin):
     inlines = [CommentInline]
     
 admin.site.register(Request,RequestAdmin)
